aae0d645ffb728c93fb7273982f42e261a326f24
IvanMenas <ivanmenasalvarenga@gmail.com>
2021-11-29 15:07:42 -0600

    Connection set successfully
    - Connection set
    - Template query

--- classes/dba-class.php
+++ classes/dba-class.php
@@ -4,18 +4,31 @@ class dbManager {
     private $db = 'oci:dbname=XE';
     private $user = 'hr';
     private $pass = 'hr01';
+    private $conn;
 
-    
+    public function __construct()
+    {
+        if($this->conn == null){
+            $this->Connect();
+        }
+    }
     public function Connect(){
         try{
-            $base = new PDO($this->db, $this-> user, $this-> pass);
-            $base -> exec("SELECT * FROM EMPLOYEES");
-
-            return $base;
+           $this-> conn = new PDO($this->db, $this-> user, $this-> pass);
+           $this-> conn ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+           return "Connection successful";
                 
-        }catch(Exception $e){
+        }catch(PDOException  $e){
             return "Error: " .$e->getMessage();
         }
+    }
+
+    //Solo para guiarse a como hacer una query
+    public function Select(){
+        $sql_stmt = $this-> conn  -> prepare("SELECT * FROM EMPLOYEES");
+        $sql_stmt->execute();
+        $result = $sql_stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
     }
 }   
 ?>
